d01ed937af0704951e5303e465205b5d3bd2d9da
airbourner <airbourner94@gmail.com>
2018-11-13 10:34:50 -0600

    Archivos con preferencia funcionando

---
+++ Prolog/classandobjmgmnt.pl
@@ -0,0 +1,58 @@
+%--------------------------------------------------------------------------------------------------
+%Operations for changing classes, objects or properties into the Knowledge Base
+%--------------------------------------------------------------------------------------------------
+change_value_object_property(Object,Property,NewValue,KB,NewKB):-
+	rm_object_property(Object,Property,KB,TemporalKB),
+	add_object_property(Object,Property,NewValue,TemporalKB,NewKB).
+change_value_object_relation(Object,Relation,NewObjectRelated,KB,NewKB):-
+	rm_object_relation(Object,Relation,KB,TemporalKB),
+	add_object_relation(Object,Relation,NewObjectRelated,TemporalKB,NewKB).
+change_value_class_property(Class,Property,NewValue,KB,NewKB):-
+	rm_class_property(Class,Property,KB,TemporalKB),
+	add_class_property(Class,Property,NewValue,TemporalKB,NewKB).
+change_value_class_relation(Class,Relation,NewClassRelated,KB,NewKB):-
+	rm_class_relation(Class,Relation,KB,TemporalKB),
+	add_class_relation(Class,Relation,NewClassRelated,TemporalKB,NewKB).
+
+%%Preferences change weights
+change_weight_object_property_preference(Object,Preference,NewWeight,KB,NewKB):-
+	rm_object_property_preference(Object,Preference,KB,TemporalKB),
+	add_object_property_preference(Object,Preference,NewWeight,TemporalKB,NewKB).
+change_weight_object_relation_preference(Object,Preference,NewWeight,KB,NewKB):-
+	rm_object_relation_preference(Object,Preference,KB,TemporalKB),
+	add_object_relation_preference(Object,Preference,NewWeight,TemporalKB,NewKB).
+change_weight_class_property_preference(Class,Preference,NewWeight,KB,NewKB):-
+	rm_class_property_preference(Class,Preference,KB,TemporalKB),
+	add_class_property_preference(Class,Preference,NewWeight,TemporalKB,NewKB).
+change_weight_class_relation_preference(Class,Preference,NewWeight,KB,NewKB):-
+	rm_class_relation_preference(Class,Preference,KB,TemporalKB),
+	add_class_relation_preference(Class,Preference,NewWeight,TemporalKB,NewKB).
+%Change the name of an object
+%
+change_object_name(Object,NewName,OriginalKB,NewKB) :-
+	changeElement(class(Class,Mother,Props,Rels,Objects),class(Class,Mother,Props,Rels,NewObjects),OriginalKB,TemporalKB),
+	isElement([id=>Object|Properties],Objects),
+	changeElement([id=>Object|Properties],[id=>NewName|Properties],Objects,NewObjects),
+	change_relations_with_object(Object,NewName,TemporalKB,NewKB).
+change_relations_with_object(_,_,[],[]).
+change_relations_with_object(Object,NewName,[class(C,M,P,R,O)|T],[class(C,M,P,NewR,NewO)|NewT]):-
+	change_relations(Object,NewName,O,NewO),
+	change_relation(Object,NewName,R,NewR),
+	change_relations_with_object(Object,NewName,T,NewT).
+change_relations(_,_,[],[]).
+change_relations(Object,NewName,[[id=>N,P,R]|T],[[id=>N,P,NewR]|NewT]):-
+	change_relation(Object,NewName,R,NewR),
+	change_relations(Object,NewName,T,NewT).
+change_relation(_,_,[],[]).
+change_relation(OldName,NewName,[[R=>OldName,Weight]|T],[[R=>NewName,Weight]|NewT]):-
+	change_relation(OldName,NewName,T,NewT).
+change_relation(OldName,NewName,[[not(R=>OldName),Weight]|T],[[not(R=>NewName),Weight]|NewT]):-
+	change_relation(OldName,NewName,T,NewT).
+change_relation(OldName,NewName,[H|T],[H|NewT]):-
+	change_relation(OldName,NewName,T,NewT).
+
+%Change the name of a class
+change_class_name(Class,NewName,KB,NewKB):-
+	changeElement(class(Class,Mother,Props,Rels,Objects),class(NewName,Mother,Props,Rels,Objects),KB,TemporalKB),
+	changeMother(Class,NewName,TemporalKB,TemporalKB2),
+	change_relations_with_object(Class,NewName,TemporalKB2,NewKB).
---
+++ Prolog/listmgmnt.pl~
@@ -0,0 +1,60 @@
+%----------------------------------------
+% List management
+%----------------------------------------
+%Change all ocurrences of an element X in a list for the value Y
+%changeElement(X,Y,InputList,OutputList).
+%Example (p,b,[p,a,p,a,y,a],[p,b,p,b,y,b])
+changeElement(_,_,[],[]).
+changeElement(X,Y,[X|T],[Y|N]):-
+	changeElement(X,Y,T,N).
+changeElement(X,Y,[H|T],[H|N]):-
+	changeElement(X,Y,T,N).
+%Delete all ocurrences of an element X in a list
+%deleteElement(X,InputList,OutputList).
+%Example (a,[p,a,p,a,y,a],[p,p,y])
+deleteElement(_,[],[]).
+deleteElement(X,[X|T],N):-
+	deleteElement(X,T,N).
+deleteElement(X,[H|T],[H|N]):-
+	deleteElement(X,T,N),
+	X\=H.
+%Verify if an element X is in a list
+%isElement(X,List)
+%Example (n,[b,a,n,a,n,a])
+isElement(X,[X|_]).
+isElement(X,[_|T]):-
+	isElement(X,T).
+%Convert in a single list a list of lists
+%Example ([[a],[b,c],[],[d]],[a,b,c,d]).
+append_list_of_lists([],[]).
+append_list_of_lists([H|T],X):-
+	append(H,TList,X),
+	append_list_of_lists(T,TList).
+%Delete all elements with a specific property in a property-value list
+%deleteAllElementsWithSameProperty(P,InputList,OutputList).
+%Example (p2,[p1=>v1,p2=>v2,p3=>v3,p2=>v4,p4=>v4],[p1=>v1,p3=>v3,p4=>v4])
+deleteAllElementsWithSameProperty(_,[],[]).
+deleteAllElementsWithSameProperty(X,[[X=>_,_]|T],N):-
+	deleteAllElementsWithSameProperty(X,T,N).
+deleteAllElementsWithSameProperty(X,[H|T],[H|N]):-
+	deleteAllElementsWithSameProperty(X,T,N).
+%%Single without weights
+deleteAllElementsWithSamePropertySingle(_,[],[]).
+deleteAllElementsWithSamePropertySingle(X,[X=>_|T],N):-
+	deleteAllElementsWithSamePropertySingle(X,T,N).
+deleteAllElementsWithSamePropertySingle(X,[H|T],[H|N]):-
+	deleteAllElementsWithSamePropertySingle(X,T,N).
+%Delete all elements with a specific negated property in a property-value list
+%deleteAllElementsWithSameNegatedProperty(P,InputList,OutputList).
+%Example (p2,[p1=>v1,not(p2=>v2),not(p3=>v3),p2=>v4,p4=>v4],[p1=>v1,not(p3=>v3),p2=>v4,p4=>v4])
+deleteAllElementsWithSameNegatedProperty(_,[],[]).
+deleteAllElementsWithSameNegatedProperty(X,[[not(X=>_),_]|T],N):-
+	deleteAllElementsWithSameNegatedProperty(X,T,N).
+deleteAllElementsWithSameNegatedProperty(X,[H|T],[H|N]):-
+	deleteAllElementsWithSameNegatedProperty(X,T,N).
+%Single version
+deleteAllElementsWithSameNegatedPropertySingle(_,[],[]).
+deleteAllElementsWithSameNegatedPropertySingle(X,[not(X=>_)|T],N):-
+	deleteAllElementsWithSameNegatedPropertySingle(X,T,N).
+deleteAllElementsWithSameNegatedPropertySingle(X,[H|T],[H|N]):-
+	deleteAllElementsWithSameNegatedPropertySingle(X,T,N).
